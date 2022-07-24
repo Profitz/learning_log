f1fd6031ccc787dfe8c919917e28ea252ef50d74
Python Ray <raygould@gmail.com>
2022-07-24 14:59:15 -0400

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-g2!#ue7q275%!wv3dsl%s#=+sbiq9h07bil2ouleerx7a1pe(0"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
