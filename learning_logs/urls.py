9d56ff93f8fa81ace9a76d5dc402f1a6abbc7a95
ProfitzTV <raygould@gmail.com>
2022-07-24 22:03:50 -0400

    resolved bug in topics listing and entry edits feature added

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,5 +1,5 @@
 """Defines URL patterns for learning_logs."""
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 
@@ -7,11 +7,14 @@ app_name = "learning_logs"
 urlpatterns = [
     # Home page
     path("", views.index, name="index"),
+    # Page that shows all topics.
     path("topics/", views.topics, name="topics"),
-    # Detail for a single topic.
+    # Detail page for a single topic.
     path("topics/<int:topic_id>/", views.topic, name="topic"),
     # Page for adding a new topic.
     path("new_topic/", views.new_topic, name="new_topic"),
     # Page for adding a new entry.
     path("new_entry/<int:topic_id>/", views.new_entry, name="new_entry"),
+    # Page for editing an entry.
+    path("edit_entry/<int:entry_id>/", views.edit_entry, name="edit_entry"),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Topic
+
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -20,7 +21,7 @@ def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
-    return render(request, "learning_logs/topics.html", context)
+    return render(request, "learning_logs/topic.html", context)
 
 
 def new_topic(request):
@@ -57,3 +58,21 @@ def new_entry(request, topic_id):
     # Display a blank or invalid form.
     context = {"topic": topic, "form": form}
     return render(request, "learning_logs/new_entry.html", context)
+
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != "POST":
+        # Initial request; pre-fill form with the current entry.
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("learning_logs:topic", topic_id=topic.id)
+    context = {"entry": entry, "topic": topic, "form": form}
+    return render(request, "learning_logs/edit_entry.html", context)
