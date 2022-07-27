3442b836723884fe2d551aa5aafe4cde5f097e14
ProfitzTV <raygould@gmail.com>
2022-07-27 15:26:04 -0400

    finalized topic/user permissions

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -135,3 +135,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# My Settings
+LOGIN_URL = "users:login"
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,5 +1,6 @@
 from statistics import mode
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Topic(models.Model):
@@ -7,6 +8,7 @@ class Topic(models.Model):
 
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Returns a string representation of the model."""
